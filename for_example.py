fedbf5ea023b6b0d9a5e5a74bdb7f7ea4e47c9ad
Francesco Dessi <fradessi21@gmail.com>
2025-01-29 16:49:54 +0100

    session 5 work

---
+++ for_example.py
@@ -0,0 +1,22 @@
+#sum the first 10 numbers
+sum = 0
+for num in range(1, 11):  #11  is excluded, so it foes from 1 to 10
+    sum = sum + num
+print(sum)
+
+#rewrite it using while loop
+sum = 0
+num = 0
+while num <= 100:
+    num = num + 1
+    sum +- num #same thing as sum = sum + 1
+    
+
+#print multiplication table
+for i in range(1,11):
+    for j in range(1,11):
+            print(f" {i} X {j} = {i*j}")
+
+i = 20
+print("i i i")
+print(f" {i} {21+i} i i ")
